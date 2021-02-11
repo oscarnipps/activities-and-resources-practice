53f987516ed17fb78b72cfca65fa35c40ac30d0f
oscarnipps <oscarnips@gmail.com>
2021-02-11 13:24:31 +0100

    added lifecycle methods

--- app/src/main/java/com/example/activities_and_resources_practice/MainActivity.java
+++ app/src/main/java/com/example/activities_and_resources_practice/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.activities_and_resources_practice;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +11,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
 }
